a0cd97a048a2d6c271a58ef966fc17bbb2a38338
Whitney Monk <whitneym92@gmail.com>
2020-06-20 11:56:55 -0500

    added joke, recipe, and recipe id ajax calls

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 var userInput = "";
 
 
-var searchRecipes = function(recipes) {
+var ajaxJoke = function() {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://joke3.p.rapidapi.com/v1/joke",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "joke3.p.rapidapi.com",
+            "x-rapidapi-key": "eb678bae5dmsh447c5c8972466f2p12564djsn27078449d11c"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+};
+
+ajaxJoke();
+
+var searchRecipes = function() {
 
     let APIkey = "2f4280fc895d40be90a0aea15ecda433";
     let queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + APIkey + "&ingredients=" + userInput;
@@ -14,4 +33,19 @@ var searchRecipes = function(recipes) {
     });
 };
 
-searchRecipes()
+searchRecipes();
+
+
+
+var ajaxRecipeId = function() {
+
+    let APIkey = "2f4280fc895d40be90a0aea15ecda433";
+    let queryUrl = "https://api.spoonacular.com/recipes/" + recipeId + "/information?apiKey=" + APIkey
+
+    $ajax ({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+};
